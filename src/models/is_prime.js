9febfb02e8ef540e6bf77eb03febc55b0ffc98bf
Stuart Milne <86377797+StrtMilne@users.noreply.github.com>
2022-02-14 17:52:05 +0000

    created RunningInstructions.md file to detail how to run the application and what I am happy/what I could improve on. Made modifications to improve efficience of prime checker.

--- src/models/is_prime.js
+++ src/models/is_prime.js
@@ -2,7 +2,11 @@ const isPrime = (numberToCheck) => {
     if(numberToCheck < 2){
         return false;
     }
-    for (let i = 2; i < numberToCheck; i++) {
+
+    // Check that all numbers up to floored square root of given number don't divide into the number
+    // Starts at 5 as getPrimes function will never need to check before that number 
+    
+    for (let i = 5; i <= Math.floor(Math.sqrt(numberToCheck)); i++) {
         if (numberToCheck % i === 0){
             
             return false;
